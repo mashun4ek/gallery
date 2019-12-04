46e9a19d79e99c7fae35e36a91eff6a1a5cef550
mashun4ek <mashun4ek@gmail.com>
2019-12-03 22:22:44 -0500

    Created the Create() method on user controller and added http methods

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/mashun4ek/webdevcalhoun/gallery/views"
@@ -17,8 +18,14 @@ type Users struct {
 	NewView *views.View
 }
 
+// renders the form where user can create a new user account
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+// create a new user account POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is create methods")
 }
