d961244b540cb7d03fb5abc5874ef847ecf342c6
mashun4ek <mashun4ek@gmail.com>
2020-01-16 12:52:23 -0500

    Add gallery service interface

--- models/gallaries.go
+++ models/gallaries.go
@@ -4,8 +4,18 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-type Gallary struct {
+type Gallery struct {
 	gorm.Model
 	UserID uint   `gorm:"not_null;index"`
 	Title  string `gorm:"not_null"`
+}
+
+type GalleryService interface{}
+
+type GalleryDB interface {
+	Create(gallery *Gallery) error
+}
+
+type galleryGorm struct {
+	db *gorm.DB
 }
